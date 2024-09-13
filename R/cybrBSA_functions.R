286f1ce624434ce5eb797790d90f9b2c09a6cd8c
Cassandra <cassandra.buzby@gmail.com>
2024-09-13 11:47:55 -0400

    Deleting deprecated functions
    If you want to find old functions, come back to the commit before this one! This was the culling.

--- R/cybrBSA_functions.R
+++ R/cybrBSA_functions.R
@@ -20,20 +20,19 @@ ChromKey <- data.frame(chromosomes = c("I", "II", "III", "IV", "V", "VI", "VII",
                                  "NC_001141.2", "NC_001142.9", "NC_001143.9", "NC_001144.5",
                                  "NC_001145.3", "NC_001146.8", "NC_001147.6", "NC_001148.4", "NC_001224.1"))
 
+ChromosomeScale <- data.frame(CHROM = factor(as.character(as.roman(1:16)),
+                                             levels = as.character(as.roman(1:16))),
+                              start = rep(1, 16),
+                              end = c(.23,.81,.32, 1.53, .58, .27, 1.09, .56, .44, .75, .67, 1.08, .92, .78, 1.09, .95)*1000000) %>%
+  pivot_longer(c(start, end), names_to = "delete", values_to = "POS") %>%
+  mutate(Summary = NA, Label = NA) %>% select(-delete)
 
-cybr_BSAcolors = c("#345F6F","#D7335C","#FFB05C",
-                   "black",
-                   "gray",
-                   "#f7dcfa",
-                   "#d0eaf5",
-                   "#cce3d6",
-                   "#fcd7df",
-                   "#a32a02")
-
-cybr_CHROMcolors =c("#F26430", "#0A369D", "#7EA3CC","#FF9F1C", "#C14953","#92DCE5", "#8FC93A","#4C4C47",
-                      "#F26430", "#0A369D", "#7EA3CC","#FF9F1C", "#C14953","#92DCE5", "#8FC93A","#4C4C47",
-                      "#F26430", "#0A369D", "#7EA3CC","#FF9F1C", "#C14953","#92DCE5", "#8FC93A","#4C4C47")
-
+ChromosomeScale2 <- data.frame(CHROM = factor(as.character(as.roman(1:16)),
+                                              levels = as.character(as.roman(1:16))),
+                               start = rep(1, 16),
+                               end = c(.23,.81,.32, 1.53, .58, .27, 1.09, .56, .44, .75, .67, 1.08, .92, .78, 1.09, .95)*1000000) %>%
+  pivot_longer(c(start, end), names_to = "delete", values_to = "POS") %>%
+  mutate(summary = 0, label = "Bulk") %>% select(-delete)
 
 theme_cybr <- function(base_size = 11,
                        base_family = "",
@@ -208,33 +207,6 @@ glm_cb2_short <- function(..., W, formula, numgroups = FALSE, outputlength = 4, 
 }
 
 ################################################################################
-#Deprecated:
-dcybr_callpeaks <- function(dataset, param = NULL, threshold = NULL){
-  dataset %>% mutate(summary = abs(summary)) -> dataset
-  #Set cutoff
-
-  Cutoff <- data.frame(X = NA)
-  if(is.null(threshold) == FALSE){
-    Cutoff$X <- threshold
-  }else{
-    dataset %>% filter(CHROM == "III") %>%
-      ungroup() %>%
-      reframe(X = max(summary)) -> Cutoff
-  }
-  #Find peaks
-  dataset %>%
-    filter(summary > Cutoff$X) %>%
-    group_by(CHROM, label) %>%
-    summarize(summary = max(summary)) %>%
-    ungroup() %>%
-    merge(dataset) -> output
-
-  if(is.null(param) == FALSE){
-    output %>% filter(label == param) -> output
-  }
-  return(output)
-}
-
 #New version:
 cybr_callpeaks <- function(dataset, param = NULL, threshold = NULL, include_all = NULL){
   dataset %>% mutate(summary = abs(summary)) %>% filter(label != "intercept",
@@ -630,172 +602,8 @@ cybrIDAlleles <- function(BSAdfstart = finaldf, Parentdf = test, yeast = TRUE){
 #### Reformat data so that it has bulk etc included
 
 
-
-
 ################################################################################
 ## FUNCTIONS NO LONGER INCLUDED
-################################################################################
-
-################################################################################
-### Run GLM Function
-
-dcybrBSA_GLM <-  function(lrP, chr = "II", windowsize = 100, formula = "PAllele ~ Bulk*Parent",
-                         resultscol = c("Intercept", "Bulk", "Parent", "Interaction")){
-
-  lrP <- subset(lrP, CHROM == chr)
-
-  AllResults <- list()
-  for(c in unique(lrP$CHROM)){
-    lrP <- subset(lrP, CHROM == c)
-    #Run the glm on that chromosome
-    Results <- foreach (i=unique(lrP$POS), .combine=rbind) %dopar% {
-      res <- suppressWarnings(glm(as.formula(formula), weights = ReadCount, family = binomial, data = lrP[lrP$POS == i,]))
-
-      #Output of foreach automatically binds rows of what is printed
-      c(c, i, summary(res)$coefficients[((length(summary(res)$coefficients)/2)+1):(length(summary(res)$coefficients) - length(summary(res)$coefficients)/4)])
-    }
-
-    resnames <- suppressWarnings(glm(as.formula(formula), weights = ReadCount, family = binomial, data = lrP[lrP$POS == unique(lrP$POS)[1],]))
-
-    #Format the results for the next function
-    Results <- as.data.frame(Results)
-    colnames(Results) <- c("CHROM", "POS", names(resnames$coefficients))
-    #colnames(Results) <- c("CHROM", "POS", resultscol)
-
-    for(i in 2:length(colnames(Results))){
-      Results[,i] <- as.numeric(Results[,i])
-    }
-    Results %>% arrange(POS) -> Results
-  }
-  return(Results)
-}
-
-################################################################################
-### New Window Script Function
-
-dcybrSmoothBSAWindows <- function(Results, windowsize = 100, chr = unique(Results$CHROM)[1]){
-
-  #extract parameters
-  Results %>% select(-CHROM, -POS) %>% names() -> params
-
-  #arrange by position
-  Results %>% filter(CHROM == chr) %>% arrange(POS) -> Results
-
-  #run window
-  WResult <- foreach(i=windowsize+1:(length(Results$POS) - (2*windowsize)), .combine=rbind) %dopar% {
-
-    smoothedparams <- vector()
-    for(p in 1:length(params)){
-      smoothedparams[p] <- mean(Results[[params[p]]][(i-windowsize):(i+windowsize)])
-    }
-
-    #print CHROM, index, POS, and mean
-    unlist(c(chr, i, Results[i,2:(2+length(params))],smoothedparams))
-  }
-
-  #rename columns
-  WResult <- as.data.frame(WResult)
-
-  colnames(WResult) <- c("CHROM", "Index", "POS",
-                         paste(params, "Z", sep = "_"),
-                         paste(params, "Zprime", sep = "_"))
-
-  #convert to numeric
-  for(i in 2:length(colnames(WResult))){
-    WResult[,i] <- as.numeric(WResult[,i])
-  }
-
-  return(WResult)
-}
-
-
-dcybrSmoothBSAWindows_b <- function(Results, windowsize = 100, chr = unique(Results$CHROM)[1]){
-
-  #extract parameters
-  Results %>% select(-CHROM, -POS) %>% names() -> params
-
-  #arrange by position
-  Results %>% filter(CHROM == chr) %>% arrange(POS) -> Results
-
-  Results %>% summarise(SNPs = length(unique(POS))) %>%
-    distinct() %>%
-    mutate(maxW = floor(SNPs/2)) -> tablecounts
-
-  if(windowsize < tablecounts$maxW){
-    #run window
-    WResult <- foreach(i=windowsize+1:(length(Results$POS) - (2*windowsize)), .combine=rbind) %dopar% {
-
-      smoothedparams <- vector()
-      for(p in 1:length(params)){
-        smoothedparams[p] <- mean(Results[[params[p]]][(i-windowsize):(i+windowsize)])
-      }
-
-      #print CHROM, index, POS, and mean
-      unlist(c(chr, i, Results[i,2:(2+length(params))],smoothedparams))
-    }
-
-    #rename columns
-    WResult <- as.data.frame(WResult)
-    colnames(WResult) <- c("CHROM", "Index", "POS",
-                           paste(params, "Z", sep = "_"),
-                           paste(params, "Zprime", sep = "_"))
-
-    #convert to numeric
-    for(i in 2:length(colnames(WResult))){
-      WResult[,i] <- as.numeric(WResult[,i])
-    }
-    return(WResult)
-  }else{
-    warning(print(paste("Window size of chromosome ", chr, "are larger than number of data points")),
-            call. = TRUE, immediate. = FALSE, noBreaks. = FALSE,
-            domain = NULL)
-  }
-}
-
-dcybrSmoothBSAWindows_Med <- function(Results, windowsize = 100, chr = unique(Results$CHROM)[1]){
-
-  #extract parameters
-  Results %>% select(-CHROM, -POS) %>% names() -> params
-
-  #arrange by position
-  Results %>% filter(CHROM == chr) %>% arrange(POS) -> Results
-
-  Results %>% summarise(SNPs = length(unique(POS))) %>%
-    distinct() %>%
-    mutate(maxW = floor(SNPs/2)) -> tablecounts
-
-  if(windowsize < tablecounts$maxW){
-    #run window
-    WResult <- foreach(i=windowsize+1:(length(Results$POS) - (2*windowsize)), .combine=rbind) %dopar% {
-
-      smoothedparams <- vector()
-      for(p in 1:length(params)){
-        smoothedparams[p] <- median(Results[[params[p]]][(i-windowsize):(i+windowsize)])
-      }
-
-      #print CHROM, index, POS, and mean
-      unlist(c(chr, i, Results[i,2:(2+length(params))],smoothedparams))
-    }
-
-    #rename columns
-    WResult <- as.data.frame(WResult)
-    colnames(WResult) <- c("CHROM", "Index", "POS",
-                           paste(params, "Z", sep = "_"),
-                           paste(params, "Zprime", sep = "_"))
-
-    #convert to numeric
-    for(i in 2:length(colnames(WResult))){
-      WResult[,i] <- as.numeric(WResult[,i])
-    }
-    return(WResult)
-  }else{
-    return(NA)
-    warning(paste("Window size of chromosome ", chr, "are larger than number of data points"),
-            call. = TRUE, immediate. = FALSE, noBreaks. = FALSE,
-            domain = NULL)
-  }
-}
-
 ################################################################################
 # Check number of loci per chromosome for samples to get window sizes
 
@@ -819,160 +627,6 @@ checkWindowLim <- function(Dataset, includechr = TRUE, exceptchr = NULL){
   return(tablecounts)
 }
 
-################################################################################
-## Make function for plotting
-
-dcybrPlotZPrime <- function(zprimedf,
-                           columns = c("Bulk_Zprime", "Parent_Zprime", "Interaction_Zprime"),
-                           chromosomes = "All",
-                           title = "Smoothed Z Scores",
-                           yeast = TRUE,
-                           colvalues = c("#345F6F", "#D7335C", "#FFB05C")){
-
-  if(yeast == TRUE){
-    ChromKey <- data.frame(chromosomes = c("I", "II", "III", "IV", "V", "VI", "VII", "VIII",
-                                           "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "M"),
-                           CHROM = c("NC_001133.9", "NC_001134.8", "NC_001135.5", "NC_001136.10",
-                                     "NC_001137.3", "NC_001138.5", "NC_001139.9", "NC_001140.6",
-                                     "NC_001141.2", "NC_001142.9", "NC_001143.9", "NC_001144.5",
-                                     "NC_001145.3", "NC_001146.8", "NC_001147.6", "NC_001148.4", "NC_001224.1"))
-
-    zprimedf$CHROM <- factor(zprimedf$CHROM, levels = ChromKey$chromosomes)
-  }
-
-
-  if(chromosomes == "All"){
-    zprimedf %>% filter(CHROM != "M") %>%
-      pivot_longer(columns, names_to = "Factor", values_to = "Zprime") %>%
-      ggplot(aes(x = POS, y = Zprime, color = Factor)) +
-
-      #Add Bulk, Parent, Day, and Interaction Traces
-      geom_line(size = 0.75) +
-      scale_color_manual(values = colvalues) +
-
-      #Add the breaks for the facets
-      scale_x_continuous(breaks = seq(from = 0, to = max(zprimedf$POS), by = 1e5), name = "Genomic Position") +
-      facet_grid(~CHROM, scales = "free_x", space = "free_x") + theme_minimal() +
-      theme(legend.position = "bottom", axis.text.x=element_blank(),
-            axis.ticks.x=element_blank()) +
-      xlab("Genomic Position") + ylab("Zprime")+
-      ggtitle(paste("Whole Genome ", title, sep = "")) -> finalplot
-
-  }else{
-
-    zprimedf %>% filter(CHROM %in% chromosomes) %>%
-      pivot_longer(columns, names_to = "Factor", values_to = "Zprime") %>%
-      ggplot(aes(x = POS, y = Zprime, color = Factor)) +
-
-      #Add Bulk, Parent, Day, and Interaction Traces
-      geom_line(size = 0.75) +
-      scale_color_manual(values = colvalues) +
-
-      #Add the breaks for the facets
-      scale_x_continuous(breaks = seq(from = 0, to = max(zprimedf$POS), by = 1e5), name = "Genomic Position") +
-      facet_grid(~CHROM, scales = "free_x", space = "free_x") + theme_minimal() +
-      theme(legend.position = "bottom", axis.text.x=element_blank(),
-            axis.ticks.x=element_blank()) +
-      xlab("Genomic Position") + ylab("Zprime")+
-      ggtitle(title) -> finalplot
-  }
-  return(finalplot)
-}
-
-dcybrPlotZScore <- function(zprimedf = CuSO4_wholegenomeBSA,
-                           column = "Bulk_Z",
-                           chromosomes = "All",
-                           title = "Z Scores by Position",
-                           yeast = TRUE,
-                           dotcolor = "black"){
-
-  if(yeast == TRUE){
-    ChromKey <- data.frame(chromosomes = c("I", "II", "III", "IV", "V", "VI", "VII", "VIII",
-                                           "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "M"),
-                           CHROM = c("NC_001133.9", "NC_001134.8", "NC_001135.5", "NC_001136.10",
-                                     "NC_001137.3", "NC_001138.5", "NC_001139.9", "NC_001140.6",
-                                     "NC_001141.2", "NC_001142.9", "NC_001143.9", "NC_001144.5",
-                                     "NC_001145.3", "NC_001146.8", "NC_001147.6", "NC_001148.4", "NC_001224.1"))
-
-    zprimedf$CHROM <- factor(zprimedf$CHROM, levels = ChromKey$chromosomes)
-  }
-
-
-  if(chromosomes == "All"){
-
-    zprimedf %>% pivot_longer(column, names_to = "Factor", values_to = "Zscore") %>%
-      filter(CHROM != "M") %>% filter(Factor == column) %>%
-      ggplot(aes(x = POS, y = Zscore)) +
-      geom_point(size = 0.5, alpha = 0.08, color = dotcolor) +
-      #Add the breaks for the facets
-      scale_x_continuous(breaks = seq(from = 0, to = max(zprimedf$POS), by = 1e5), name = "Genomic Position") +
-      facet_grid(~CHROM, scales = "free_x", space = "free_x") + theme_minimal() +
-      theme(legend.position = "bottom", axis.text.x=element_blank(),
-            axis.ticks.x=element_blank()) +
-      xlab("Genomic Position") + ylab("Zscore") +
-      ggtitle(title) -> finalplot
-
-  }else{
-
-    zprimedf %>% pivot_longer(column, names_to = "Factor", values_to = "Zscore") %>%
-      filter(CHROM %in% chromosomes) %>% filter(Factor == column) %>%
-      ggplot(aes(x = POS, y = Zscore)) +
-      geom_point(size = 1, alpha = 0.08, color = dotcolor) +
-      #Add the breaks for the facets
-      scale_x_continuous(breaks = seq(from = 0, to = max(zprimedf$POS), by = 1e5), name = "Genomic Position") +
-      facet_grid(~CHROM, scales = "free_x", space = "free_x") + theme_minimal() +
-      theme(legend.position = "bottom", axis.text.x=element_blank(),
-            axis.ticks.x=element_blank()) +
-      xlab("Genomic Position") + ylab("Zscore") +
-      ggtitle(title) -> finalplot
-  }
-  return(finalplot)
-}
-
-################################################################################
-# Adding the loci within a window instead of smoothing
-
-dcybrBSA_GLM_window <-  function(lrP, chr = "II", windowsize = 5000, formula = "PAllele~Bulk*Parent"){
-  require(stringr)
-  if(identical(grep(" ", formula), integer(0)) == FALSE){return(print("Remove spaces from formula or use cybrBSA_GLM()"))}
-
-
-  lrP <- subset(lrP, CHROM == chr)
-
-  AllResults <- list()
-  for(c in unique(lrP$CHROM)){
-    lrP <- subset(lrP, CHROM == c)
-    #Run the glm on that chromosome
-    Results <- foreach (i=unique(lrP$POS), .combine=rbind) %dopar% {
-      windowdata <- lrP[lrP$POS < (i+windowsize) & lrP$POS > (i-windowsize),]
-
-      #Convert to sum of counts
-      mycols <- unlist(str_split(unlist(str_split(formula, pattern = c("~"))), pattern = "\\*"))
-      windowdata %>% group_by(.[mycols]) %>% summarize(ReadCount = sum(ReadCount)) -> windowdata
-
-      res <- suppressWarnings(glm(as.formula(formula),
-                                  weights = ReadCount,
-                                  family = binomial,
-                                  data = windowdata))
-
-      #Output of foreach automatically binds rows of what is printed
-      c(c, i, summary(res)$coefficients[((length(summary(res)$coefficients)/2)+1):(length(summary(res)$coefficients) - length(summary(res)$coefficients)/4)])
-    }
-
-    resnames <- suppressWarnings(glm(as.formula(formula), weights = ReadCount, family = binomial, data = lrP[lrP$POS == unique(lrP$POS)[1],]))
-
-    #Format the results for the next function
-    Results <- as.data.frame(Results)
-    colnames(Results) <- c("CHROM", "POS", names(resnames$coefficients))
-
-    for(i in 2:length(colnames(Results))){
-      Results[,i] <- as.numeric(Results[,i])
-    }
-    Results %>% arrange(POS) -> Results
-  }
-  return(Results)
-
-}
 
 ################################################################################
 # Additional functions that are probably in this now
@@ -1024,20 +678,6 @@ cybr2_rollmean <- function(dataframe){
   dataframe %>% pivot_longer(c(-CHROM, -POS), names_to = "label") %>% group_by(CHROM, label) %>% arrange(POS) %>%
     summarize(POS = POS, CHROM = CHROM, SmoothCount = ceiling(frollmean(value, n = 100))) %>% na.omit() %>% pivot_wider(names_from = label,values_from = SmoothCount)
 }
-
-ChromosomeScale <- data.frame(CHROM = factor(as.character(as.roman(1:16)),
-                                             levels = as.character(as.roman(1:16))),
-                              start = rep(1, 16),
-                              end = c(.23,.81,.32, 1.53, .58, .27, 1.09, .56, .44, .75, .67, 1.08, .92, .78, 1.09, .95)*1000000) %>%
-  pivot_longer(c(start, end), names_to = "delete", values_to = "POS") %>%
-  mutate(Summary = NA, Label = NA) %>% select(-delete)
-
-ChromosomeScale2 <- data.frame(CHROM = factor(as.character(as.roman(1:16)),
-                                              levels = as.character(as.roman(1:16))),
-                               start = rep(1, 16),
-                               end = c(.23,.81,.32, 1.53, .58, .27, 1.09, .56, .44, .75, .67, 1.08, .92, .78, 1.09, .95)*1000000) %>%
-  pivot_longer(c(start, end), names_to = "delete", values_to = "POS") %>%
-  mutate(summary = 0, label = "Bulk") %>% select(-delete)
 
 ############################ PERMUTATIONS ######################################
 
